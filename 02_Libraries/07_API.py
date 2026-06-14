82fc55a499a3813e9c69ffaefed357bd93d7e455
Anik <anik.khondokar09@gmail.com>
2026-06-15 02:20:03 +0600

    Add API interaction script using requests library

---
+++ 02_Libraries/07_API.py
@@ -0,0 +1,30 @@
+import requests #To make https requests
+import sys # for commandline argument
+import json # comes with python
+
+if len(sys.argv)!=2:
+    sys.exit()
+
+response=requests.get("https://itunes.apple.com/search?entity=song&limit=50&term="+sys.argv[1])
+#print(json.dumps(response.json(),indent=2))
+
+o =response.json() # o is a object
+for results in o["results"]: # results is a list in that object
+    print(results["trackName"]) # trackName is one of the keys
+
+'''
+1. What is API? How can we use it?
+2. requests library allows to web requests.
+3. Documentation : https://pypi.org/project/requests/
+4. JSON
+5. https://itunes.apple.com/search?entity=song&limit=1&term=weezer 
+if i type this on browser it will download a file in JSON formet which is some 
+information form the apple itunes API
+6. Now, if i want to access this data with python i need to write program
+which can access the server's data in this case my python program will patents to be 
+a browser.
+7.we need to know API formet to manipulate or get something form the apple API
+
+
+
+'''
